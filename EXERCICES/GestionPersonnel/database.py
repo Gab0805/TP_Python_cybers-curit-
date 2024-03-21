314af442128e62afc50b955fcd85fea0870c3d5b
Gab0805 <djibrilnd@yahoo.fr>
2024-03-21 19:22:48 -0400

    first commit

--- EXERCICES/GestionPersonnel/database.py
+++ EXERCICES/GestionPersonnel/database.py
@@ -11,12 +11,12 @@ database= "ecole"
 cursor = connect_db.cursor() # curseur qui permet d'accéder à la bd
 
 def insertion(): # insertion dans la table etudiant
-    sql_insert = "INSERT INTO ETUDIANT (code_permanent,nom,prenom,date_naissance,specialite) VALUES ('jit111','jin','talla','1998-01-09','info')"
+    sql_insert = "INSERT INTO ETUDIANT (code_permanent,nom,prenom,date_naissance,specialite) VALUES ('min123','jin','talla','1998-01-09','info')"
     cursor.execute(sql_insert)
     connect_db.commit()
 
 def afficher():
-    sql_afficher = "SELECT * FROM ETUDIANT" # ou "SELECT (code_permanent,nom,prenom,date_naissance,specialite) FROM ETUDIANT"
+    sql_afficher = "SELECT * FROM ETUDIANT" # ou SELECT (code_permanent,nom,prenom,date_naissance,specialite) FROM ETUDIANT
     cursor.execute(sql_afficher)
     etudiants = cursor.fetchall() # récupération des données 
 
