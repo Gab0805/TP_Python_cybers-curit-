d3731e849dbb6de2fc72ff52d845203c96cd3c52
Gab0805 <djibrilnd@yahoo.fr>
2024-03-21 20:13:23 -0400

    first commit

--- EXERCICES/GestionPersonnel/databaseprof_db.py
+++ EXERCICES/GestionPersonnel/databaseprof_db.py
@@ -3,22 +3,32 @@ import mysql.connector as mysql
 #initialisation de la connexion a la bd
 def connect_db():
     db = mysql.connect(
-        host= "localhost",
-        user= "root",
-        password= "",
-        database= "ecole"
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "ecole"
 
     )
 
-    cursor = db.cursor() # curseur qui permet d'accéder à la bd
-    return cursor
+    return db
 
 #Insertion
-def insertion(): # insertion dans la table etudiant
+def insertion(code_permanent,nom,prenom,date_naissance,specialite): # insertion dans la table etudiant
     sql_insert = "INSERT INTO ETUDIANT (code_permanent,nom,prenom,date_naissance,specialite) VALUES (%s,%s,%s,%s,%s)"
-    cursor = connect_db()
-    infos = (cursor.lastrowid, code_permanent,nom,prenom,date_naissance,specialite)
-    cursor.execute(sql_insert, infos)
+    db = connect_db()
+    cursor = db.cursor()
+    parametre = (code_permanent,nom,prenom,date_naissance,specialite)
+    cursor.execute(sql_insert, parametre)
+    db.commit()
+    
+# tester l'insertion
+code_permanent = input("donner le code permanent")
+nom = input("donner le nom")
+prenom = input("donner le prénom")
+date_naissance = input("donner la date de naissance")
+specialite = input("donner la spécialité")
+
+insertion(code_permanent,nom,prenom,date_naissance,specialite)
 
 
 
